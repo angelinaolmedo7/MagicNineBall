ead2b9db176716221635691a29b997583482df09
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-12-11 18:35:09 -0800

    Finished tutorial

--- MagicNineBall/ViewController.swift
+++ MagicNineBall/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
+
     
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var shakeButton: UIButton!
@@ -23,7 +25,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     @IBAction func shakeButtonTapped(_ sender: Any) {
-        print("Shake it like a polaroid picture!")
+        generateAnswer()
+    }
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
+
+        generateAnswer()
+    }
+    
+    func generateAnswer() {
+        let randomIndex = Int.random(in: 0..<answers.count)
+
+        answerLabel.text = answers[randomIndex]
     }
 }
 
