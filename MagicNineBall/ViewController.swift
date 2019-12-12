e3319f0183ca1d69d0440e27224cc1e61c1df7f1
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-12-11 18:28:50 -0800

    Shake It! Button

--- MagicNineBall/ViewController.swift
+++ MagicNineBall/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var shakeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    @IBAction func shakeButtonTapped(_ sender: Any) {
+        print("Shake it like a polaroid picture!")
+    }
 }
 
